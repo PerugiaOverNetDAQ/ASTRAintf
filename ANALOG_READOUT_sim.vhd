08dfa63c339a5c10ee356886e50a24021154d483
MatteoDAntonioCoder <matteo.dantonio@gmail.com>
2021-11-29 14:13:10 +0100

    Aggiunta degli assert di errore

--- ANALOG_READOUT_sim.vhd
+++ ANALOG_READOUT_sim.vhd
@@ -27,7 +27,7 @@ entity ANALOG_READOUT_sim is
     iMUX_SHIFT_CLK        : in  std_logic;
     iHOLD         				: in  std_logic;
     iMUX_READ_RESET       : in  std_logic;
-    oMUX_OUT		     		  : out std_logic_vector(15 downto 0)
+    oMUX_OUT		     		  : out std_logic_vector(7 downto 0)
     );
 end ANALOG_READOUT_sim;
 
@@ -36,31 +36,35 @@ end ANALOG_READOUT_sim;
 architecture Behavior of ANALOG_READOUT_sim is
 
   --!Number of channel selected
-  signal    sMuxOut : std_logic_vector(15 downto 0);
+  signal    sMuxOut : std_logic_vector(7 downto 0);
   --!Channel index
   signal    i       : natural;
   --!MUX time constraints (response time of the MUX)
   constant  t1      : time := 3 ns;
 
 begin
-  --!Output delay introduced by the MUX
+  --!Introduced a delay on the output
   oMUX_OUT  <= transport sMuxOut after t1;
   
-  -- Questo e quello
-    assert (iMUX_READ_RESET = '0')
-    report "Questo non va bene"
+  --!Set iHOLD before iMUX_READ_RESET
+    assert (iHOLD = '1' and iMUX_READ_RESET = '1')
+    report "An Attempt was made to set iMUX_READ_RESET before iHOLD"
     severity ERROR;
-  mux_shift_sim : process (iMUX_SHIFT_CLK, iHOLD, iMUX_READ_RESET)
+  mux_shift_sim : process
 	begin
-    sMuxOut <= (others => 'Z'); 
+    sMuxOut <= (others => 'Z');
 		wait until iHOLD'event and iHOLD='0';
-    
-		wait until rising_edge(iMUX_READ_RESET);
-		i <= 0;    
+		wait until iMUX_READ_RESET'event and iMUX_READ_RESET='1';
+		i <= 0;
     mux_out : while (i < cFE_CHANNELS) loop
-      wait until rising_edge(iMUX_SHIFT_CLK);
+      wait until iMUX_SHIFT_CLK'event and iMUX_SHIFT_CLK='1';
       sMuxOut <= std_logic_vector(to_unsigned(i, sMuxOut'length));
       i <= i + 1;
+      
+      --!Incomplete Transaction
+      assert (iHOLD = '1' and iMUX_READ_RESET = '0')
+      report "An Attempt was made to modify iHOLD or iMUX_READ_RESET before that the transaction was completed"
+      severity ERROR;
     end loop mux_out;    
 	end process;
 
