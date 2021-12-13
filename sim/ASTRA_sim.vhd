046d42b8507cf8341e8bd5b3908640a026f3da0b
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2021-12-13 14:21:48 +0100

    Add simulation folder

---
+++ sim/ASTRA_sim.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use ieee.math_real.all;
+
+use work.basic_package.all;
+use work.ASTRApackage.all;
+
+
+entity ASTRA_sim is
+
+end entity ASTRA_sim;
+
+architecture behav of ASTRA_sim is
+
+begin
+
+end architecture behav;
